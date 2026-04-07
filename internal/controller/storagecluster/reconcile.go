4349746b8b4723ba3de3d67525720a84ffbe78e0
ShravaniVangur <shravanivangur@gmail.com>
2026-04-07 23:35:26 +0530

    rbac: add nonResourceURLs for metrics endpoints to operator ClusterRole
    The operator creates the ocs-metrics-reader ClusterRole which grants
    access to /metrics, /healthz, and /readyz non-resource URLs.
    
    Signed-off-by: ShravaniVangur <shravanivangur@gmail.com>

--- internal/controller/storagecluster/reconcile.go
+++ internal/controller/storagecluster/reconcile.go
@@ -126,6 +126,7 @@ var storageClusterFinalizer = "storagecluster.ocs.openshift.io"
 // +kubebuilder:rbac:groups=security.openshift.io,resources=securitycontextconstraints,resourceNames=hostnetwork-v2,verbs=use
 // +kubebuilder:rbac:groups=csiaddons.openshift.io,resources=networkfenceclasses,verbs=get;list;watch;create;update;delete
 // +kubebuilder:rbac:groups=csiaddons.openshift.io,resources=csiaddonsnodes,verbs=get;list
+// +kubebuilder:rbac:urls=/metrics;/healthz;/readyz,verbs=get
 // +kubebuilder:rbac:groups=console.openshift.io,resources=consoleplugins,verbs=get;watch;update
 
 // Reconcile reads that state of the cluster for a StorageCluster object and makes changes based on the state read
